c29b4ec9deeb9e835b7ba1fc43b2ae9e79dbc5fe
Burak Bulut <lotheas@gmail.com>
2022-09-30 22:46:43 +0300

    Proje Olusturuldu

---
+++ src/main/java/kodlama/io/carRental/business/CarManager.java
@@ -0,0 +1,7 @@
+package kodlama.io.carRental.business;
+
+import kodlama.io.carRental.dataAccess.CarDao;
+
+public class CarManager {
+    private CarDao cardao;
+}
---
+++ src/main/java/kodlama/io/carRental/entities/Car.java
@@ -0,0 +1,12 @@
+package kodlama.io.carRental.entities;
+
+public class Car {
+    private int id;
+    private int brandId;
+    private int colorId;
+    private String modelYear;
+    private double dailyPrice;
+    private String description;
+
+
+}
